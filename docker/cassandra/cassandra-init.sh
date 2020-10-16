23e68b3a50e3badf9c717638c59223b9842c7d0d
Shrinivaasan Venkataramani <svaasan@gmail.com>
2020-10-16 13:48:56 -0500

    Dockerfile added

---
+++ docker/cassandra/cassandra-init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CQL="CREATE KEYSPACE IF NOT EXISTS my_retail WITH REPLICATION = { 'class' : 'org.apache.cassandra.locator.SimpleStrategy','replication_factor': '1'};
+
+DROP TABLE my_retail.product_detail;
+CREATE TABLE IF NOT EXISTS my_retail.product_detail  (
+    id        int,
+    current_price text,
+    PRIMARY KEY (id)
+);
+INSERT INTO my_retail.product_detail (id, current_price) VALUES (13860428, '{\"value\": 14.49,\"currency_code\":\"USD\"}');
+INSERT INTO my_retail.product_detail (id, current_price) VALUES (16483589, '{\"value\": 15.49,\"currency_code\":\"USD\"}');
+INSERT INTO my_retail.product_detail (id, current_price) VALUES (16696652, '{\"value\": 16.49,\"currency_code\":\"USD\"}');
+INSERT INTO my_retail.product_detail (id, current_price) VALUES (16752456, '{\"value\": 10.49,\"currency_code\":\"USD\"}');
+INSERT INTO my_retail.product_detail (id, current_price) VALUES (15643793, '{\"value\": 3.49,\"currency_code\":\"USD\"}');
+"
+
+until echo $CQL | cqlsh; do
+  echo "cqlsh: Cassandra is unavailable - retry later"
+  sleep 2
+done &
+
+exec /docker-entrypoint.sh "$@"
